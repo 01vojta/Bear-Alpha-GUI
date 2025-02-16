047c12c0b25efc990b68d30a7457cff9ae0dca71
01vojta <142297012+01vojta@users.noreply.github.com>
2025-02-16 22:49:26 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,3 +3,4 @@ local cloneref = cloneref or function(o) return o end
 COREGUI = cloneref(game:GetService("CoreGui"))
 Players = cloneref(game:GetService("Players"))
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/01vojta/Bear-Alpha-GUI/refs/heads/main/LoaderMain.lua"))()
